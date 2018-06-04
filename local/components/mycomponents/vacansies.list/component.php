4e9246374d082bad7b0490ccd3911bf39ef63a5d
Vlad Barskov <barskov@reaspekt.ru>
2018-06-04 15:55:55 +0300

    Added normally cache, added component_epilog.php(for normal working jquerry with cache)

--- local/components/mycomponents/vacansies.list/component.php
+++ local/components/mycomponents/vacansies.list/component.php
@@ -16,54 +16,66 @@ if(!isset($arParams["CACHE_TIME"]))
     $arParams["CACHE_TIME"] = 360000;
 
 //Включаем кеширование
-$this->StartResultCache(false, ($arParams["CACHE_GROUPS"]==="N"? false: $USER->GetGroups()));
+if($this->StartResultCache(false, ($arParams["CACHE_GROUPS"]==="N"? false: $USER->GetGroups()))) {
 
-//Собираем Имя и ID разделов инфоблока
-$rs_Section = CIBlockSection::GetList(array(), array('IBLOCK_ID' => intval($arParams['IBLOCKS']['0'])), false, array('NAME', 'ID'));
-while ($ar_Section = $rs_Section->GetNext()) {
-    $arResult["SECTIONS_STUFF"][] = array(  
-        'MY_SECTION_ID' => $ar_Section['ID'], 
-        'MY_SECTION_NAME' => $ar_Section['NAME'],
-    ); 
-};
+	//Собираем Имя и ID разделов инфоблока
+	$rs_Section = CIBlockSection::GetList(array(), array('IBLOCK_ID' => intval($arParams['IBLOCKS']['0'])), false, array('NAME', 'ID'));
+	while ($ar_Section = $rs_Section->GetNext()) {
+	    $arResult["SECTIONS_STUFF"][] = array(  
+	        'MY_SECTION_ID' => $ar_Section['ID'], 
+	        'MY_SECTION_NAME' => $ar_Section['NAME'],
+	    ); 
+	};
 
-//Собираем нужные параметры элементов инфоблока
-$arSelect = Array("ID", "NAME", "DETAIL_TEXT", "PROPERTY_VAC_STAZH", "PROPERTY_VAC_GRAPH", "PROPERTY_VAC_EDU", "IBLOCK_SECTION_ID");
-$arFilter = Array("IBLOCK_ID"=> intval($arParams['IBLOCKS']['0']), "ACTIVE"=>"Y");
-$res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
-while($arFields = $res->GetNext()) {
-	$arResult["ITEMS_STUFF"][] = array(
-		'MY_ELEMENT_ID' => $arFields['ID'],
-		'MY_ELEMENT_NAME' => $arFields['NAME'],
-		'MY_ELEMENT_DETAIL_TEXT' => $arFields['DETAIL_TEXT'],
-		'MY_ELEMENT_PROPERTY_VAC_STAZH' => $arFields['PROPERTY_VAC_STAZH_VALUE'],
-		'MY_ELEMENT_PROPERTY_VAC_GRAPH' => $arFields['PROPERTY_VAC_GRAPH_VALUE'],
-		'MY_ELEMENT_PROPERTY_VAC_EDU' => $arFields['PROPERTY_VAC_EDU_VALUE'],
-		'MY_ELEMENT_SECTION_ID' => $arFields['IBLOCK_SECTION_ID']
-	);
-};
+	//Собираем нужные параметры элементов инфоблока
+	$arSelect = Array("ID", "NAME", "DETAIL_TEXT", "PROPERTY_VAC_STAZH", "PROPERTY_VAC_GRAPH", "PROPERTY_VAC_EDU", "IBLOCK_SECTION_ID");
+	$arFilter = Array("IBLOCK_ID"=> intval($arParams['IBLOCKS']['0']), "ACTIVE"=>"Y");
+	$res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+	while($arFields = $res->GetNext()) {
+		$arResult["ITEMS_STUFF"][] = array(
+			'MY_ELEMENT_ID' => $arFields['ID'],
+			'MY_ELEMENT_NAME' => $arFields['NAME'],
+			'MY_ELEMENT_DETAIL_TEXT' => $arFields['DETAIL_TEXT'],
+			'MY_ELEMENT_PROPERTY_VAC_STAZH' => $arFields['PROPERTY_VAC_STAZH_VALUE'],
+			'MY_ELEMENT_PROPERTY_VAC_GRAPH' => $arFields['PROPERTY_VAC_GRAPH_VALUE'],
+			'MY_ELEMENT_PROPERTY_VAC_EDU' => $arFields['PROPERTY_VAC_EDU_VALUE'],
+			'MY_ELEMENT_SECTION_ID' => $arFields['IBLOCK_SECTION_ID']
+		);
+	};
 
-// формируем дерево
-$arResult["TREE"] = array();
-foreach ($arResult["SECTIONS_STUFF"] as $keySect) {
-	$arResult["TREE"][$keySect['MY_SECTION_NAME']] = array();
-	foreach ($arResult["ITEMS_STUFF"] as $keyItem) {
-		if ($keySect["MY_SECTION_ID"] == $keyItem["MY_ELEMENT_SECTION_ID"]) {
-			$arResult["TREE"][$keySect['MY_SECTION_NAME']][] = array(
-				'MY_ELEMENT_ID' => $keyItem['MY_ELEMENT_ID'],
-				'MY_ELEMENT_NAME' => $keyItem['MY_ELEMENT_NAME'],
-				'MY_ELEMENT_DETAIL_TEXT' => $keyItem['MY_ELEMENT_DETAIL_TEXT'],
-				'MY_ELEMENT_PROPERTY_VAC_STAZH' => $keyItem['MY_ELEMENT_PROPERTY_VAC_STAZH'],
-				'MY_ELEMENT_PROPERTY_VAC_GRAPH' => $keyItem['MY_ELEMENT_PROPERTY_VAC_GRAPH'],
-				'MY_ELEMENT_PROPERTY_VAC_EDU' => $keyItem['MY_ELEMENT_PROPERTY_VAC_EDU']
-			);
+	// формируем дерево
+	$arResult["TREE"] = array();
+	foreach ($arResult["SECTIONS_STUFF"] as $keySect) {
+		$arResult["TREE"][$keySect['MY_SECTION_NAME']] = array();
+		foreach ($arResult["ITEMS_STUFF"] as $keyItem) {
+			if ($keySect["MY_SECTION_ID"] == $keyItem["MY_ELEMENT_SECTION_ID"]) {
+				$arResult["TREE"][$keySect['MY_SECTION_NAME']][] = array(
+					'MY_ELEMENT_ID' => $keyItem['MY_ELEMENT_ID'],
+					'MY_ELEMENT_NAME' => $keyItem['MY_ELEMENT_NAME'],
+					'MY_ELEMENT_DETAIL_TEXT' => $keyItem['MY_ELEMENT_DETAIL_TEXT'],
+					'MY_ELEMENT_PROPERTY_VAC_STAZH' => $keyItem['MY_ELEMENT_PROPERTY_VAC_STAZH'],
+					'MY_ELEMENT_PROPERTY_VAC_GRAPH' => $keyItem['MY_ELEMENT_PROPERTY_VAC_GRAPH'],
+					'MY_ELEMENT_PROPERTY_VAC_EDU' => $keyItem['MY_ELEMENT_PROPERTY_VAC_EDU']
+				);
+			};
 		};
 	};
+
+	//Записываем данные в кэш, и подключаем template
+	$this->SetResultCacheKeys(array(
+		"TREE"
+	));
+	$this->IncludeComponentTemplate();
 };
 
-//Записываем данные в кэш, и подключаем template
-$this->SetResultCacheKeys(array(
-	"TREE"
-));
-$this->IncludeComponentTemplate();
+/*if(
+	$arResult["LAST_ITEM_IBLOCK_ID"] > 0
+	&& $USER->IsAuthorized()
+	&& $APPLICATION->GetShowIncludeAreas()
+	&& CModule::IncludeModule("iblock")
+)
+{
+	$arButtons = CIBlock::GetPanelButtons($arResult["LAST_ITEM_IBLOCK_ID"], 0, 0, array("SECTION_BUTTONS"=>false));
+	$this->addIncludeAreaIcons(CIBlock::GetComponentMenu($APPLICATION->GetPublicShowMode(), $arButtons));
+}*/
 ?>
--- local/components/mycomponents/vacansies.list/templates/.default/template.php
+++ local/components/mycomponents/vacansies.list/templates/.default/template.php
@@ -1,5 +1,4 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<?CJSCore::Init(array("jquery"));?>
 
 <?foreach($arResult['TREE'] as $sectName => $itemValue): ?>
 <div class="block">
